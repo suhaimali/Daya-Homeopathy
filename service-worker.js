9d6991390d203bc05db733e149d022de08b728d4
suhaim soft <alivpsuhaim@gmail.com>
2026-01-01 10:43:31 +0530

    fix bug

--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,11 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open("daya-cache-v1").then(cache => {
       return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json",
-        "/icons/icon-192x192.png",
-        "/icons/icon-512x512.png"
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./img/logo.png",
+        "./icons/icon-512x512.png"
       ]);
     })
   );
