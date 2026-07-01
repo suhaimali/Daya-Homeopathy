7c60868c793dcd29d0fe32648f068569addc798d
suhaim soft <alivpsuhaim@gmail.com>
2026-07-01 16:16:23 +0530

    Update map location

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -20,7 +20,7 @@ const Location = () => {
           {/* MAP — full-width tall card */}
           <div className="loc-map-card">
             <iframe
-              src="https://maps.google.com/maps?q=Pathappiriyam,+Edavanna,+Malappuram,+Kerala&t=&z=14&ie=UTF8&iwloc=&output=embed"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3913.8583824087104!2d76.13407297452228!3d11.198112151275142!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba6379e41a4084d%3A0xfc0d03ac283d758f!2sDaya%20Homeopathy%20Pathappiriyam!5e0!3m2!1sen!2snl!4v1782902727196!5m2!1sen!2snl"
               width="100%"
               height="100%"
               style={{ border: 0, display: 'block' }}
